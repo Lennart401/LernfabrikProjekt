a113ed66981966ca157893ba196b4f8cb8e8a850
Lennart401 <lennart.popkes@gmail.com>
2021-01-24 22:27:01 +0100

    Remove CoreM4 for now - might be added later - communication between cores causes two much trouble right now

--- FullProject.ino
+++ FullProject.ino
@@ -1,20 +1,18 @@
 #include <Arduino.h>
-#include <RPC_internal.h>
 
 #include "CoreM7.h"
-#include "CoreM4.h"
+//#include "CoreM4.h"
 
 void setup() {
     Serial.begin(115200);
-    RPC1.begin();
 
     #ifdef CORE_CM7
         core_m7::Setup();
     #endif
 
-    #ifdef CORE_CM4
-        core_m4::Setup();
-    #endif
+    //#ifdef CORE_CM4
+    //    core_m4::Setup();
+    //#endif
 }
 
 void loop() {
@@ -22,7 +20,7 @@ void loop() {
         core_m7::Loop();
     #endif
 
-    #ifdef CORE_CM4
-        core_m4::Loop();
-    #endif
+    //#ifdef CORE_CM4
+    //    core_m4::Loop();
+    //#endif
 }
