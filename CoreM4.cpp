e4ba68fffd3e81a3c75e52485c6702fd8b68599f
Lennart401 <lennart.popkes@gmail.com>
2021-06-23 14:51:46 +0200

    Add LVGL demo on M4 core

--- CoreM4.cpp
+++ CoreM4.cpp
@@ -4,16 +4,146 @@
 
 #include <Arduino.h>
 #include <RPC_internal.h>
+#include <lvgl.h>
+#include <stdint.h>
+#include <SPI.h>
+#include <Adafruit_ILI9341.h>
+#include <lv_examples.h>
+#include <mbed.h>
+#include <InterruptIn.h>
+#include <RotaryEncoder.h>
+#include <rtos.h>
+
+#define TFT_CS 7
+#define TFT_DC 6
+
+#define COLOR_BUFFER_SIZE LV_HOR_RES_MAX * 20
+
+#define PIN_A 13
+#define PIN_B 14
+#define ENCODER_BUTTON A6
 
 namespace core_m4 {
 
-void setup() {
+static RotaryEncoder encoder(PIN_A, PIN_B, RotaryEncoder::LatchMode::FOUR3);
+static mbed::InterruptIn encoderInterruptA(digitalPinToPinName(PIN_A)); // (digitalPinToPinName(PIN_A), PullUp);
+static mbed::InterruptIn encoderInterruptB(digitalPinToPinName(PIN_B)); // (digitalPinToPinName(PIN_B), PullUp);
+static int encoderLastPosition = 0;
 
+static Adafruit_ILI9341 tft(TFT_CS, TFT_DC);
+
+static lv_disp_buf_t disp_buf;
+
+static lv_color_t buf_1[COLOR_BUFFER_SIZE];
+static lv_color_t buf_2[COLOR_BUFFER_SIZE];
+
+static lv_obj_t *label;
+
+static rtos::Thread sendThread(osPriorityNormal);
+
+static void runSendThread() {
+    while (true) {
+        RPC1.println("SET mode/running 1");
+        rtos::ThisThread::sleep_for((uint32_t) 1000);
+    }
+}
+
+static void checkPosition() {
+    encoder.tick();
+}
+
+static void my_flush_cb(lv_disp_drv_t *disp_drv, const lv_area_t *area, lv_color_t *color_p) {
+    uint32_t w = (area->x2 - area->x1 + 1);
+    uint32_t h = (area->y2 - area->y1 + 1);
+
+    tft.startWrite();
+    tft.setAddrWindow(area->x1, area->y1, w, h);
+    tft.writePixels(&color_p->full, w * h, true, true);
+    tft.endWrite();
+
+    lv_disp_flush_ready(disp_drv);
+}
+
+static bool encoder_read(lv_indev_drv_t *drv, lv_indev_data_t *data) {
+    encoder.tick();
+    int pos = encoder.getPosition();
+
+    int16_t diff = static_cast<int16_t>(pos) - static_cast<int16_t>(encoderLastPosition);
+    data->enc_diff = diff;
+    
+    if (pos != encoderLastPosition) {
+        //Serial.println("Encoder Position: " + String(pos) + " with diff: " + String(diff));
+        //RPC1.println("Encoder Position: " + String(pos) + " with diff: " + String(diff));
+    }
+    
+    encoderLastPosition = pos;
+
+    bool isButtonStateLow = (digitalRead(ENCODER_BUTTON) == LOW);
+
+    if (isButtonStateLow) {
+        data->state = LV_INDEV_STATE_PR;
+        //Serial.println("BUTTON PRESSED");
+        //RPC1.println("Button pressed");
+    } else {
+        data->state = LV_INDEV_STATE_REL;
+    }
+
+    return false;
+}
+
+void setup() {
+    // input initialization
+    encoderInterruptA.rise(&checkPosition);
+    encoderInterruptA.fall(&checkPosition);
+
+    encoderInterruptB.rise(&checkPosition);
+    encoderInterruptB.fall(&checkPosition);
+
+    pinMode(ENCODER_BUTTON, INPUT_PULLUP);
+
+    // thread initialization
+    sendThread.start(runSendThread);
+
+    // lvgl stuff
+    lv_init();
+
+    // tft initialization
+    tft.begin(60000000);
+    tft.setRotation(1);
+
+    // display buffer
+    lv_disp_buf_init(&disp_buf, buf_1, buf_2, COLOR_BUFFER_SIZE);
+
+    // display driver
+    lv_disp_drv_t disp_drv;
+    lv_disp_drv_init(&disp_drv);
+    disp_drv.flush_cb = my_flush_cb;
+    //disp_drv.gpu_fill_cb = gpu_fill;
+    //disp_drv.gpu_blend_cb = gpu_blend;
+    disp_drv.buffer = &disp_buf;
+    lv_disp_t *disp;
+    disp = lv_disp_drv_register(&disp_drv);
+
+    // touch screen driver
+    lv_indev_drv_t indev_drv;
+    lv_indev_drv_init(&indev_drv);
+    indev_drv.type = LV_INDEV_TYPE_ENCODER;
+    indev_drv.read_cb = encoder_read;
+    lv_indev_drv_register(&indev_drv);
+
+    // lvgl init done, setup widgets now
+    //label = lv_label_create(lv_scr_act(), NULL);
+    //lv_label_set_text(label, "WICHSER!");
+    //lv_obj_align(label, NULL, LV_ALIGN_CENTER, 0, 0);
+    
+    
+    //lv_demo_benchmark();
+    lv_demo_keypad_encoder();
 }
 
 void loop() {
-    RPC1.println("SET running 1");
-    delay(1000);
+    lv_task_handler();
+    rtos::ThisThread::sleep_for((uint32_t) 1);
 }
 
 }
