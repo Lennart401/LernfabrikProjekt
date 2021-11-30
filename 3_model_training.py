7a4a6f3fa38b189b23a83214912dd7452c7f3d9e
Lennart401 <lennart.popkes@gmail.com>
2021-11-30 16:38:53 +0100

    Update 3_model_training.py and add new model

--- 3_model_training.py
+++ 3_model_training.py
@@ -11,9 +11,22 @@ from preprocessing import preprocessing, featureengineering
 from util import plotter, io, paths, constants
 
 # ----------------------------------------------------------------------------------------------------------------------
+# Settings
+# If running this script as a whole, these are the settings to modify
+RESAMPLE = True
+EPOCHS = 50
+LEARNING_RATE = 0.01
+
+PRINT_INTERMEDIATE_RESULTS = False
+
+# ----------------------------------------------------------------------------------------------------------------------
 # 1. Preprocessing
 # We start by importing the samples and doing the known preprocessing on them and also immediate generating the features
 all_movements = io.load_all_movements(paths.MOVEMENTS_PATH)
+
+if RESAMPLE:
+    all_movements = preprocessing.resample_dataset(all_movements)
+
 all_movements = preprocessing.normalize_time(all_movements)
 all_features = featureengineering.generate_basic_features(all_movements)
 
@@ -25,36 +38,36 @@ all_features = featureengineering.generate_basic_features(all_movements)
 # Note that call convert_dict_to_dataset will translate the movement type id into the 'array index form' using a lookup
 # table. This has the benefit that no matter what movement types we use, the model will see n classes from 0 to n-1.
 # After a model prediction, you would apply the sample LUT in reverse to extract the actual movement type predicted.
-x, y = preprocessing.convert_dict_to_dataset(all_features)
-print(x)
-print(y)
+X, y = preprocessing.convert_dict_to_dataset(all_features)
+if PRINT_INTERMEDIATE_RESULTS:
+    print(X)
+    print(y)
 
 # Next, the split the data into a training set and into a test set for model evaluation. We use a train size of 70%
 # and a random state for reproducable results. Feel free to change these values to whatever you need.
-x_train, x_test, y_train, y_test = preprocessing.split_train_test(x, y, train_size=0.7, random_state=10, stratify=y)
-print(x_test)
+X_train, X_test, y_train, y_test = preprocessing.split_train_test(X, y, train_size=0.7, random_state=10, stratify=y)
+if PRINT_INTERMEDIATE_RESULTS:
+    print(X_test)
+
+# Next, we split the training data into the actual training set and a validation set, used for training the model an
+# checking the score.
+X_train, X_val, y_train, y_val = preprocessing.split_train_test(X_train, y_train, train_size=0.7, random_state=10,
+                                                                stratify=y_train)
 
 # Before we one-hot-encode our labels, let's have a look at another problem in our data: there is a strong class
 # imbalance. This is not so much a problem for our test data but very much so for our training data. These are the
 # number of samples for each class:
-classes, counts = np.unique(y_train, return_counts=True)
-for i in classes:
-    print(f'Class "{constants.LUT_INDEX_TO_NAME[i]}" ({i}): {counts[i]}')
+if PRINT_INTERMEDIATE_RESULTS:
+    classes, counts = np.unique(y_train, return_counts=True)
+    for i in classes:
+        print(f'Class "{constants.LUT_INDEX_TO_NAME[i]}" ({i}): {counts[i]}')
 
 # What we can do now it to and upsample all classes to the one with the most samples:
-x_train, y_train = preprocessing.oversample_dataset(x_train, y_train)
+X_train, y_train = preprocessing.oversample_dataset(X_train, y_train)
 
 # Now let's look at the counts again (but a bit less verbose):
-print(np.unique(y_train, return_counts=True)[1])
-
-# Next, we split the training data into the actual training set and a validation set, used for training the model an
-# checking the score.
-#
-# You would technically do this train-validation-split before oversampling, but since we do not have enough data before
-# oversample, for now, we have to do this here.
-# TODO move this before oversampling when we have enough samples
-x_train, x_val, y_train, y_val = preprocessing.split_train_test(x_train, y_train, train_size=0.7, random_state=10,
-                                                                stratify=y_train)
+if PRINT_INTERMEDIATE_RESULTS:
+    print(np.unique(y_train, return_counts=True)[1])
 
 # Each element in the y-arrays is the movement type as an integer. However, we don't want our labels in this form.
 # For the model, we need our labels to be one-hot encoded. In this case, this means that every 'label' is an array of
@@ -73,7 +86,8 @@ categories = [list(constants.LUT_INDEX_TO_NAME.keys())]
 y_train_enc = preprocessing.one_hot_encode_labels(y_train, categories=categories)
 y_val_enc = preprocessing.one_hot_encode_labels(y_val, categories=categories)
 y_test_enc = preprocessing.one_hot_encode_labels(y_test, categories=categories)
-print(y_test_enc)
+if PRINT_INTERMEDIATE_RESULTS:
+    print(y_test_enc)
 
 # ----------------------------------------------------------------------------------------------------------------------
 # 2. Machine Learning
@@ -86,8 +100,9 @@ print(y_test_enc)
 # The second layer has rectified linear unit activation function (i.e. f(x) = max(0, x)) and the last layer has a
 # softmax activation function. This means that the probabilities of all classes add up to one.
 model = tf.keras.Sequential([
-    tf.keras.layers.Input(shape=[len(x_train[0])]),
-    tf.keras.layers.Dense(7, activation=tf.keras.activations.relu),
+    tf.keras.layers.Input(shape=[len(X_train[0])]),
+    tf.keras.layers.Dense(10, activation=tf.keras.activations.relu),
+    tf.keras.layers.Dense(15, activation=tf.keras.activations.relu),
     tf.keras.layers.Dense(len(y_train_enc[0]), activation=tf.keras.activations.softmax)
 ])
 
@@ -101,23 +116,22 @@ model = tf.keras.Sequential([
 #                   loss=tf.keras.losses.CategoricalCrossentropy(),
 #                   metrics=['accuracy'])
 # 
-# TODO: what the hell is up with this learning rate?!?
+# TODO: what the hell is up with this learning rate?!? --> see learning rate optimizer in training.py
 # 
 # Next, we fit the model to our training data. To get a stable model, we train multiple models and take the best one.
 # This ensures that we do not mistake a good model for a worse one, just because the score is bad by random.
 # 
 # The function returns the fitted model and the training history for that model.
-EPOCHS = 50
 model, history = training.train_stable_model(model=model,
-                                             optimizer=tf.keras.optimizers.Adam(0.018),
+                                             optimizer=tf.keras.optimizers.Adam(LEARNING_RATE),
                                              loss=tf.keras.losses.CategoricalCrossentropy(),
                                              metrics=['accuracy'],
-                                             x_train=x_train,
+                                             x_train=X_train,
                                              y_train_enc=y_train_enc,
                                              epochs=EPOCHS,
-                                             x_test=x_test,
+                                             x_test=X_test,
                                              y_test_enc=y_test_enc,
-                                             x_val=x_val,
+                                             x_val=X_val,
                                              y_val_enc=y_val_enc,
                                              optimize='test',
                                              n=10)
@@ -128,16 +142,16 @@ model, history = training.train_stable_model(model=model,
 # ----------------------------------------------------------------------------------------------------------------------
 # 3. Model Evaluation
 # Let's look at the model evaluation on the training data:
-print('Train evaluation:', model.evaluate(x_train, y_train_enc, verbose=2))
+print('Train evaluation:', model.evaluate(X_train, y_train_enc, verbose=2))
 
 # And let's also evaluate the model on the test data:
-print('Test evaluation:', model.evaluate(x_test, y_test_enc, verbose=2))
+print('Test evaluation:', model.evaluate(X_test, y_test_enc, verbose=2))
 
 # Finally, let's create some plots for the model. Will be creating plot model history and a confusion matrix. Let's
 # start by creating the confusion matrix. We will need to un-one-hot-encode the labels in order to create a confusion
 # matrix:
-pred_train = [np.argmax(x) for x in model.predict(x_train)]
-pred_test = [np.argmax(x) for x in model.predict(x_test)]
+pred_train = [np.argmax(x) for x in model.predict(X_train)]
+pred_test = [np.argmax(x) for x in model.predict(X_test)]
 
 cm_train = tf.math.confusion_matrix(y_train, pred_train)
 cm_test = tf.math.confusion_matrix(y_test, pred_test)
@@ -148,5 +162,5 @@ plotter.plot_confusion_matrix(cm_test, classes=constants.LUT_MOVEMENT_ID_TO_NAME
 # Last but not least, let's create a plot of the model's history, to see if the need more/less epochs.
 plotter.plot_model_history(history, num_epochs=EPOCHS, use_validation_values=True)
 
-if input("save model?") == "y":
+if input("save model? ") == "y":
     tf.saved_model.save(model, "./tmp/final_model")
