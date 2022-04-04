beca55e0cb360a9c46a08c8e1ec721b84b9ce9db
Lennart401 <lennart.popkes@gmail.com>
2022-04-04 18:35:04 +0200

    data-collector | Add basic UI using tkinter

--- data-collector/main.py
+++ data-collector/main.py
@@ -1,6 +1,8 @@
 from realtime_tracker import RealtimeTracker
+from adminwindow import AdminWindow
 
 import paho.mqtt.client as mqtt
+import tkinter as tk
 import dataparser
 import dbconnector
 
@@ -16,9 +18,18 @@ def on_message(connect_client, userdata, msg):
     payload = msg.payload.decode('utf-8')
 
     print(f'Received message: {topic} - {payload}')
+
+    # get and insert
     box_id, movement_type = dataparser.parse_movement_message(topic, payload)
     dbconnector.insert_movement_report(db_con, box_id, movement_type)
     rttracker.feed_movement_report(box_id, movement_type)
+
+    # update window
+    state = rttracker.get_state(box_id)
+    transition = rttracker.get_transition(box_id)[1]
+    ui.update(box_id, state, transition)
+
+    # display states and transitions
     stset = rttracker.get_all_states_and_transition()
     dbconnector.insert_stset(db_con, stset)
     print(rttracker.state_summary())
@@ -27,6 +38,8 @@ def on_message(connect_client, userdata, msg):
 if __name__ == '__main__':
     rttracker = RealtimeTracker()
     db_con = dbconnector.open_connection()
+    root = tk.Tk()
+    ui = AdminWindow(root)
 
     client = mqtt.Client()
     client.on_connect = on_connect
